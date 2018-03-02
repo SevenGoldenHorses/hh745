d7186561a30830d12aa547bfad3e7795c468a8dc
Federico Di Gesù <federicodigesu@MBP-di-Federico.lan>
2018-03-02 18:28:22 +0100

    sistemato chiave segreta

--- Key_secret.js
+++ Key_secret.js
@@ -1,7 +1,7 @@
 module.exports = {
   consumer_key:         'Ld75JSCVOwwytcoIZhjdLc0tU',
   consumer_secret:      'Cbbmljc4w4lTTKDiFk1EK80I1pcZMYhOEBcAvPOJBxeYb1Fvk6',
-  access_token:         '927630032708755456-F9eEM5TUfbQTKL4pEKxGrFUNGxpLqpmj',
+  access_token:         '927630032708755456-F9eEM5TUfbQTKL4pEKxGrFUNGxpLqpm',
   access_token_secret:  'dcBjCcOvgicHZH0HLT6zkCP4TPVU6ACajchWqROL7oPvS',
   // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
